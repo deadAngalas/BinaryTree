9bf1f1efc01b0083e37018b28d902d912ef02297
Vladislav <vladik.rjazancevs@gmail.com>
2023-11-08 18:00:08 +0200

    new functions count and find added

--- binaryTree_1.cpp
+++ binaryTree_1.cpp
@@ -49,6 +49,16 @@ Node *CreateNode()
   return newNode;
 }
 
+void PrintTree(Node *cur) // LVR
+{
+  if(cur != NULL)
+  {
+    PrintTree(cur->left);
+    cout << cur->data << " ";
+    PrintTree(cur->right);
+  }
+}
+
 // preorder; VLR
 // inorder; LVR
 // postorder; LRV
@@ -112,51 +122,55 @@ Node *AddNode(Node *root)
     cur->right = newNode;
   }
 
-/*
-  // for left loop
-  if(newNode->data < cur->data)
-  {
-    while(newNode->data < cur->data && cur->left != NULL) // work until new data is smaller than current data && current left child is something
-    {
-      cur = cur->left; // go under left child
-    }
-    // when loop is gone, it means that it is last child and under is nothing -> new if, which again compare datas values to decide which side to add new value
-    if(newNode->data >= cur->data) // if new data value is larger or equivallent -> needs to add in right child
-    {
-      cur->right = newNode;
-    }
-    else cur->left = newNode; // if smaller than in left child
-  }
-  // for right loop
-  else if(newNode->data >= cur->data)
-  {
-    if(newNode->data >= cur->data)
-    {
-      while(newNode->data >= cur->data && cur->right != NULL)
-      {
-        cur = cur->right; // go under right child
-      }
-      if(newNode->data < cur->data) // if new data value is smaller -> needs to add in left child
-      {
-        cur->left = newNode;
-      }
-      else cur->right = newNode; // if larger than in right child
-    }
-  }
-*/
   return 0;
+}
+
+Node *FindNode(Node *root)
+{
+  int x;
+  Node *cur = new Node;
+  cur = root;
+
+  cout << "Enter which Node need to find: ";
+  cin >> x;
+
+  while((cur->data > x && cur->left != NULL) || (cur->data <= x && cur->right != NULL) && cur->data != x)
+  {
+    while(cur->data > x && cur->left != NULL && cur->data != x)
+    {
+      cur = cur->left;
+    }
+    while(cur->data <= x && cur->right != NULL && cur->data != x)
+    {
+      cur = cur->right;
+    }
+  }
+
+  if(cur->data == x)
+  {
+    cout << "\nSuccessfully Found! :)\n";
+  }
+  else cout << "\nSorry! There is no your Node! :(\n";
+
+  return root;
+}
+
+int NodesCount(Node *cur)
+{
+  if(cur != NULL)
+  {
+    return 1 + NodesCount(cur->left) + NodesCount(cur->right);
+  }
+  else return 0;
 }
 
 int main()
 {
   Node *root = NULL;
-  enum KEYS { num1 = 49, num2 = 50, num3 = 51 , num4 = 52, num5 = 53, num6 = 54, num7 = 55, num8 = 56, num9 = 57, num10 = 45, num11 = 61, num12 = 112, num13 = 91, num14 = 93 };
+  enum KEYS { num1 = 49, num2 = 50, num3 = 51 , num4 = 52, num5 = 53, num6 = 54, num7 = 55, num8 = 56, num9 = 57, num10 = 45, num11 = 61 };
   /*
   num10 = 45; it is   -
   num11 = 61; it is   =
-  num12 = 112; it is  p
-  num13 = 91; it is   [
-  num14 = 93; it is   ]
   */
   int choice;
 
@@ -172,23 +186,22 @@ int main()
       cout << "\n\t\tMENU \n\n";
       cout << "-------------- CREATE --------------\n";
       cout << "1. Create Root\n";
-      cout << "\n-------------- PRINT  --------------\n";
-      cout << "2. Print Root value\n";
-      cout << "3. Print PreOrder (VLR)\n";
-      cout << "4. Print InOrder (LVR)\n";
-      cout << "5. Print PostOrder (LRV)\n";
       cout << "\n--------------- ADD ----------------\n";
-      cout << "6. Add Node\n";
+      cout << "2. Add Node\n";
+      cout << "\n-------------- PRINT  --------------\n";
+      cout << "3. Print Root value\n";
+      cout << "4. Print Tree\n";
+      cout << "5. Print PreOrder (VLR)\n";
+      cout << "6. Print InOrder (LVR)\n";
+      cout << "7. Print PostOrder (LRV)\n";
+      cout << "\n--------------- FIND ---------------\n";
+      cout << "8. Find Node\n";
       cout << "\n-------------- REMOVE --------------\n";
-      cout << "7. \n";
-      cout << "8. \n";
-      cout << "9. \n";
-      cout << "10.  (-)\n";
-      cout << "11.  (=)\n";
-      cout << "12.  (p)\n";
-      cout << "13.  ([)\n";
+      cout << "9. Remove Node\n";
+      cout << "\n-------------- COUNT ---------------\n";
+      cout << "10. Count How many Nodes (-)\n";
       cout << "\n-------------- STOP --------------\n";
-      cout << "14. Stop Program (])\n\n";
+      cout << "11. Stop Program (=)\n\n";
       cout << "-----------------------------------\n\n";
 
       choice = getch();
@@ -204,7 +217,16 @@ int main()
           else cout << "Root already exist!\n";
           break;
         }
-        case num2:// print root value
+        case num2: // add node
+        {
+          if(root)
+          {
+            AddNode(root);
+          }
+          else cout << "Tree does not exists!\n";
+          break;
+        }
+        case num3:
         {
           if(root)
           {
@@ -216,7 +238,18 @@ int main()
           system("pause>nul");
           break;
         }
-        case num3: // print tree
+        case num4:
+        {
+          if(root)
+          {
+            cout << "Tree elements:\n";
+            PrintTree(root);
+          }
+          else cout << "Tree does not exists!\n";
+          system("pause>nul");
+          break;
+        }
+        case num5:
         {
           if(root)
           {
@@ -224,10 +257,9 @@ int main()
             PreOrder(root);
           }
           else cout << "Tree does not exists!\n";
-          system("pause>nul");
           break;
         }
-        case num4:
+        case num6:
         {
           if(root)
           {
@@ -235,9 +267,10 @@ int main()
             InOrder(root);
           }
           else cout << "Tree does not exists!\n";
+          system("pause>nul");
           break;
         }
-        case num5:
+        case num7:
         {
           if(root)
           {
@@ -248,61 +281,40 @@ int main()
           system("pause>nul");
           break;
         }
-        case num6:
+        case num8: // find
         {
           if(root)
           {
-            AddNode(root);
+            root = FindNode(root);
           }
           else cout << "Tree does not exists!\n";
           system("pause>nul");
           break;
         }
-        case num7:
+        case num9: // remove
         {
 
           system("pause>nul");
           break;
         }
-        case num8:
+        case num10: // count
         {
-
-          system("pause>nul");
-          break;
-        }
-        case num9:
-        {
-
-          system("pause>nul");
-          break;
-        }
-        case num10:
-        {
-
+          if(root)
+          {
+            int count = NodesCount(root);
+            cout << "There is " << count << " Node/s in the tree!\n";
+          }
+          else cout << "Tree does not exists!\n";
           system("pause>nul");
           break;
         }
         case num11:
         {
-
-          system("pause>nul");
-          break;
+          cout << "\nProgram is stopped! Goodbye!"; ProgEnd(); break;
         }
-        case num12:
-        {
-
-          system("pause>nul");
-          break;
-        }
-        case num13:
-        {
-          system("pause>nul");
-          break;
-        }
-        case num14: cout << "\nProgram is stopped! Goodbye!"; ProgEnd(); break;
         default: cout << "Incorrect input!\n";
       }
-    } while(choice != num14);
+    } while(choice != num11);
 
   system("pause>nul");
   return 0;
